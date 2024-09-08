8abd70da79c271d98f9730d1bcb9ca12273f1a10
Eng-Mohamed <mkayta2023@gmail.com>
2024-09-08 20:00:33 +0300

    I done this tip calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,8 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+let bill= 380
+let tip= bill*15/100
+let tip2=bill*20/100
+if(bill<=400 && bill>=50){
+    console.log("the bill was  "+bill +"  ,the tip was  "+tip+"  and total value is  " + (bill+tip) )
+}else {
+    console.log(console.log("the bill was  "+bill +"  ,the tip was  "+tip2+"  and total value is  " + (bill+tip2) ))
+}
